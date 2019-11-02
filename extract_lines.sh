41c6586284877ead63564feb5f7ec7fa672023c5
Noémie <noemie.lehuby@zaclys.net>
2019-11-02 15:03:48 +0100

    remove trains

--- extract_lines.sh
+++ extract_lines.sh
@@ -8,11 +8,10 @@ osm_transit_extractor -i data.osm.pbf
 
 mkdir output
 
-cp osm-transit-extractor_lines.csv output/lines.csv
-cp osm-transit-extractor_stop_points.csv output/stops.csv
-
-#transform to geojson
-cat osm-transit-extractor_lines.csv|xsv select 1-8 |xsv search -s shape '^$' -v > lines_with_shapes.csv
+cat osm-transit-extractor_lines.csv|xsv select 1-8 |xsv search -s mode 'ferry|bus' |xsv search -s shape '^$' -v > lines_with_shapes.csv
 ogr2ogr output/lines.geojson -dialect sqlite -sql "SELECT *, GeomFromText(shape) FROM lines_with_shapes" lines_with_shapes.csv -a_srs "WGS84"
+
+cp lines_with_shapes.csv output/lines.csv
+cp osm-transit-extractor_stop_points.csv output/stops.csv
 
 ogr2ogr output/stops.geojson -dialect sqlite -sql "SELECT *, GeomFromText('POINT(' || lon || ' ' || lat || ')') FROM stops" output/stops.csv -a_srs "WGS84"
